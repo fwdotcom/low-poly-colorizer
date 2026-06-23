5442c03736a015b1ad4857012cf04773244dbb60
Frank Winter <studio@frankwinter.com>
2026-06-23 09:36:04 +0200

    Update project description in build_addon.py for consistency

--- build_addon.py
+++ build_addon.py
@@ -1,7 +1,8 @@
 # SPDX-FileCopyrightText: 2026 Frank Winter <https://www.frankwinter.com/>
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# This file is part of Low Poly Colorizer (LPC), a WASDCAT Games project. <https://www.wasdcat.com/>
+# This file is part of Low Poly Colorizer (LPC). <https://github.com/wasdcat/low-poly-colorizer>
+# A WASDCAT Games project. <https://www.wasdcat.com/>
 
 """Builds an installable addon zip into dist/.
 
